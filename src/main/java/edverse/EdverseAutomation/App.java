0dc9d250d7f761de5027dd407a8f22b99f08211c
viGupta <vinay.sky182@gmail.com>
2024-03-16 19:35:41 +0530

    First Commit

---
+++ src/main/java/edverse/EdverseAutomation/App.java
@@ -0,0 +1,13 @@
+package edverse.EdverseAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
